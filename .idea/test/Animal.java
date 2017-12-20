82cf001e7bd0bd875941c28f9eeda60403506bc7
yecz <1042048635@qq.com>
2017-12-20 15:57:32 +0800

    test

---
+++ .idea/test/Animal.java
@@ -0,0 +1,33 @@
+package cn.com.doone.tx.cloud.ThreadStatic;
+
+/**
+ * @OAuthor: YeCongZhi
+ * @Description:
+ * @CreatedDate: 2017/12/18 12:36
+ * @Package:cn.com.doone.tx.cloud.Thread
+ */
+public abstract class Animal extends Thread{
+
+    public double length=20;//比赛的长度
+
+    public abstract void runing();//抽象方法需要子类实现
+
+    //在父类重写run方法，在子类只要重写running方法就可以了
+    @Override
+    public void run() {
+        super.run();
+        while (length>0) {
+            runing();
+        }
+    }
+
+    //在需要回调数据的地方（两个子类需要），声明一个接口
+    public static interface Calltoback{
+        public void win();
+    }
+
+    //2.创建接口对象
+    public Calltoback calltoback;
+
+}
+
